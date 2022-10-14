566c1f7e0a53b636a30818f5b87b463462cdd335
Dazhia Blassingame <dayblassingame@gmail.com>
2022-10-14 18:17:05 -0400

    task adds to list when entered

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 import ('./App.scss');
+
 export function App(){
+    //state to hold todo list items
+    const [todos, setTodos] = useState(['wash dishes', 'wash car']);
+    const initialTodoList = todos.map((todoItem) =>
+        <li>{todoItem}</li>
+    );
+
+    const newTask =(e)=>{
+        e.preventDefault();
+        const newTask = document.getElementById('task').value
+
+        //if no task is entered do nothing
+        if(newTask== ""){
+            return;
+        }
+
+        //if task is entered add to list and display under todo section
+        setTodos(newTask + todos);
+        const todoList = document.getElementById("todo");
+        const newTodoLi = document.createElement('li');
+        newTodoLi.innerHTML = newTask;
+        todoList.appendChild(newTodoLi);
+    }
+
     return(
         <div className="main_container">
             <header>
@@ -8,13 +32,17 @@ export function App(){
             </header>
             <container className='sub_container'>
                 <h2>Add a new task</h2>
-                <input className='sub_container-input' type='text'/>
-                <input type='submit'/>
+                <form onSubmit={newTask}>
+                    <input className='sub_container-input' id='task' type='text'/>
+                    <input type='submit'/>
+                </form>
             </container>
             <container className='flexbox_container'>
                 <div className='flexbox_container-item'>
                     <h4 className='flexbox_container-item-header'>To-Do</h4>
-                    <ul id="todo"></ul>
+                    <ul id="todo">
+                        {initialTodoList}
+                    </ul>
                 </div>
                 <div className='flexbox_container-item'>
                     <h4 className='flexbox_container-item-header'>In Progress</h4>
