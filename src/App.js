af4e506d59c6024a962e43c6b3f1d6e37fc39b56
Dazhia Blassingame <dayblassingame@gmail.com>
2022-10-16 11:04:53 -0400

    task input bar styled and background

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import ('./App.scss');
+import logo from './logo.png';
 
 export function App(){
     //state to hold todo list items
@@ -19,6 +20,7 @@ export function App(){
         const todoList = document.getElementById("todo");
         const newTodoLi = document.createElement('li');
         const newTodoLiButton = document.createElement('button');
+        newTodoLiButton.classList.add('flexbox_container-listBtn');
 
         newTodoLiButton.innerHTML = newTask.value;
         newTodoLiButton.addEventListener("dblclick", moveToInProgress);
@@ -45,6 +47,8 @@ export function App(){
         newInProgressLiButton.innerHTML = newTaskInProgress;
         newInProgressLiButton.addEventListener("dblclick", moveToCompleted);
         newInProgressLiButton.addEventListener("touchend", moveToCompleted);
+        newInProgressLiButton.classList.add('flexbox_container-listBtn');
+
 
         newInProgressLi.appendChild(newInProgressLiButton);
         inProgressList.appendChild(newInProgressLi);
@@ -61,6 +65,7 @@ export function App(){
         const completedList = document.getElementById("completed");
         const completedLi = document.createElement('li');
         const completedLiButton = document.createElement('button');
+        completedLiButton.classList.add('flexbox_container-listBtn');
 
         completedLiButton.innerHTML = newTaskCompleted;
         completedLi.appendChild(completedLiButton);
@@ -68,37 +73,38 @@ export function App(){
     }
 
     return(
-        <div className="main_container">
-            <header>
-                <h1 className='logo'>To-do List</h1>
+        <div>
+            <header className='header'>
+                <div className='logo_container'><img className= 'logo_img' src={logo}/></div>
             </header>
-            <div className='sub_container'>
-                <h2>Add a new task</h2>
-                <form onSubmit={newTask}>
-                    <input className='sub_container-input' id='task' type='text'/>
-                    <input type='submit'/>
-                </form>
-            </div>
-            <div className='flexbox_container'>
-                <div className='flexbox_container-item'>
-                    <h4 className='flexbox_container-item-header'>To-Do</h4>
-                    <ul id="todo">
-                        {todos.map((todo,index) => {
-                            return (
-                                <li className="todoList_item" key={index}><button onTouchEnd={moveToInProgress} onDoubleClick={moveToInProgress}>{todo}</button></li>
-                            );
-                        })}                    
-                    </ul>
+            <main className="main_content">
+                <div className='sub_container'>
+                    <form onSubmit={newTask}>
+                        <input className='sub_container-input' id='task' type='text' placeholder='Enter new task'/>
+                        <input className='sub_container-submit' type='submit'/>
+                    </form>
                 </div>
-                <div className='flexbox_container-item'>
-                    <h4 className='flexbox_container-item-header'>In Progress</h4>
-                    <ul id="inProgress"></ul>
+                <div className='flexbox_container'>
+                    <div className='flexbox_container-item'>
+                        <h4 className='flexbox_container-item-header'>To-Do</h4>
+                        <ul id="todo">
+                            {todos.map((todo,index) => {
+                                return (
+                                    <li className="todoList_item" key={index}><button className='flexbox_container-listBtn' onTouchEnd={moveToInProgress} onDoubleClick={moveToInProgress}>{todo}</button></li>
+                                );
+                            })}                    
+                        </ul>
+                    </div>
+                    <div className='flexbox_container-item'>
+                        <h4 className='flexbox_container-item-header'>In Progress</h4>
+                        <ul id="inProgress"></ul>
+                    </div>
+                    <div className='flexbox_container-item'>
+                        <h4 className='flexbox_container-item-header'>Completed</h4>
+                        <ul id="completed"></ul>
+                    </div>
                 </div>
-                <div className='flexbox_container-item'>
-                    <h4 className='flexbox_container-item-header'>Completed</h4>
-                    <ul id="completed"></ul>
-                </div>
-            </div>
+            </main>
         </div>
     );
 }
